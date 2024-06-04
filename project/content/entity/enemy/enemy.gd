65489ef99c0f4b5bdeb8d73085aa421aa57c14f3
Emilia Bill <Bill_E@tu-berlin.de>
2024-06-04 21:37:53 +0200

    added player healthbar and enemy damage

--- project/content/entity/enemy/enemy.gd
+++ project/content/entity/enemy/enemy.gd
@@ -1,7 +1,7 @@
 class_name Enemy
 extends CharacterBody2D
 
-@onready var stats = $StatsComponent
+@onready var stats_component = $StatsComponent
 
 func get_collision_damage():
-	return stats.collision_damage
+	return stats_component.collision_damage
