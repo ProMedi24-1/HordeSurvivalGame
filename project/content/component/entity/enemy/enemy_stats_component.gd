3db634f83782b4b884ceb40ef1dcb878141abf28
Emilia <Bill_E@tu-berlin.de>
2024-06-16 17:06:54 +0200

    added shield weapon and enemy damage

--- project/content/component/entity/enemy/enemy_stats_component.gd
+++ project/content/component/entity/enemy/enemy_stats_component.gd
@@ -3,4 +3,4 @@ extends StatsComponent
 
 @export var melee_damage: int = 5 # Damage when player touches Enemy
 @export var melee_cooldown: float = 0.5 # in Seconds
-
+@export var texture: Texture
---
+++ project/content/component/entity/shared/weapon_stats_component.gd
@@ -0,0 +1,6 @@
+class_name WeaponStatsComponent
+extends Node2D
+
+@export var melee_cooldown: float = 0.5 
+@export var melee_damage: float = 20
+
